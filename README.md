5ea4916dd57997fa9c4ec2054d145dc4a517f1dd
明佑 <>
2021-09-10 17:21:04 +0800

    Setup build system with cmake and git submodules

---
+++ README.md
@@ -0,0 +1,2 @@
+CMake reference:
+https://www.foonathan.net/2016/07/cmake-dependency-handling/
--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ then
 fi
 
 echo "Building source files..."
-g++ -std=c++17 $SRC_DIR/*.cpp $SRC_DIR/*/*.cpp $EXT_DIR/*.cpp -o $DIST_DIR/AtmosphericEngine -I$EXT_DIR -I$EXT_DIR/entt/src -I/usr/local/include/bullet -I/opt/local/include -L/opt/local/lib -lGLEW -lGLFW -lbulletsoftbody -lbulletdynamics -llinearmath -lbulletcollision -llua -framework OpenGL
+g++ -std=c++17 $SRC_DIR/*.cpp $SRC_DIR/*/*.cpp $EXT_DIR/*.cpp -o $DIST_DIR/AtmosphericEngine -I/opt/local/include -I$EXT_DIR -I/usr/local/include/bullet -L/opt/local/lib -lGLEW -lGLFW -lbulletsoftbody -lbulletdynamics -llinearmath -lbulletcollision -llua -framework OpenGL
 if [ $COMPLETE = "true" ]
 then
     echo ""

9724475e59700e8db3b6fdb61c1ce832cf742cb3
loicchen <910041@gmail.com>
2025-12-09 05:29:33 +0800

    feat: serialized scene demo

--- Example_CSB/main.cpp
+++ Example_CSB/main.cpp
@@ -24,7 +24,7 @@ class CSBDemo : public Application {
             mainCamera->Yaw(-glm::half_pi<float>());
         }
 
-        loadedScene = sceneLoader->Load("assets/scenes/test.csb", glm::vec3(0.0f), CanvasLayer::LAYER_WORLD);
+        loadedScene = sceneLoader->Load("assets/scenes/Canvas.csb", glm::vec3(0.0f), CanvasLayer::LAYER_WORLD);
         if (loadedScene.success) {
             console.Info(fmt::format("CSB loaded successfully! {} nodes created", loadedScene.allNodes.size()));
         } else {
